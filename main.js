9023e6533214e1b975917af266c9898d424c1c63
Michael <mtrapani27@gmail.com>
2022-01-26 21:41:08 -0500

    dom.js added

--- main.js
+++ main.js
@@ -15,21 +15,21 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
         console.log(activeURL);
 
         chrome.storage.sync.get('siteLog', function (data) {
-            let parent = document.getElementById('main-table');
+            //let parent = document.getElementById('main-table');
             console.log()
             if (prevURL != undefined) {
               let visitLength = Date.now() - startTime;
               if (data['siteLog'][prevURL]) {
                   data['siteLog'][prevURL] += visitLength;
-                  let currentRow = document.getElementById(prevURL);
-                  currentRow.innerHTML = `<div>${prevURL}</div><div>${data['siteLog'][prevURL]}</div>`
+                  //let currentRow = document.getElementById(prevURL);
+                  //currentRow.innerHTML = `<div>${prevURL}</div><div>${data['siteLog'][prevURL]}</div>`
               } else {
                   data['siteLog'][prevURL] = visitLength;
-                  let currentRow = document.createElement('div');
-                  currentRow.setAttribute('class', 'site-row');
-                  currentRow.setAttribute('id', prevURL);
-                  currentRow.innerHTML = `<div>${prevURL}</div><div>${data['siteLog'][prevURL]}</div>`
-                  parent.appendChild(currentRow);
+                  //let currentRow = document.createElement('div');
+                  //currentRow.setAttribute('class', 'site-row');
+                  //currentRow.setAttribute('id', prevURL);
+                  //currentRow.innerHTML = `<div>${prevURL}</div><div>${data['siteLog'][prevURL]}</div>`
+                  //parent.appendChild(currentRow);
                  
               }
             }
