e02ecf5e22eea31b3e15b91f549ce64fed2208cb
Michael <mtrapani27@gmail.com>
2022-01-26 19:33:05 -0500

    update

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({siteLog});
     console.log(`Site log created: ${siteLog}`);
   });
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    console.log(activeInfo.tabId);
+});
