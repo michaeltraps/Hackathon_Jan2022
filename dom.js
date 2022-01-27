a0f498d66a5a37228df6a0acac79e4d1a76a5791
Michael <mtrapani27@gmail.com>
2022-01-26 22:18:59 -0500

    update

--- dom.js
+++ dom.js
@@ -1,3 +1,21 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    alert("Test");
+    let parent = document.getElementByID('main-table');
+    parent.innerHTML = "";
+    // Loop through each element in our object, and insert a row/value
+    chrome.storage.sync.get('siteLog', function (data) {
+        currentData = data['siteLog'];
+
+        for (const site of currentData) {
+            let newItem = document.createElement('div');
+            newItem.setAttribute('class', 'site-row');
+            newItem.innerHTML(`<div>${site}</div><div>${currentData[site]}</div>`);
+            parent.appendChild(newItem);
+        }
+
+        
+    });
+
+
   });
