25c44e71508138e2310265f9afdfc0b945061a48
Michael <mtrapani27@gmail.com>
2022-01-26 22:28:22 -0500

    commit

--- dom.js
+++ dom.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.sync.get('siteLog', function (data) {
         currentData = data['siteLog'];
         for (const site of Object.keys(currentData)) {
-            alert(Array.from(Object.keys(currentData)).toString());
             let newItem = document.createElement('div');
             newItem.setAttribute('class', 'site-row');
             newItem.innerHTML = `<div>${site}</div><div>${currentData[site]}</div>`;
