f6da1fc01e1994561926bf76ce5fc05d32c43609
Michael <mtrapani27@gmail.com>
2022-01-26 20:34:52 -0500

    stuff

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 let siteLog = {};
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({siteLog});
+    chrome.storage.sync.set({ 'siteLog': siteLog });
     console.log(`Site log created: ${siteLog}`);
-  });
-
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    var activeURL = activeTab.url.match("\/\/(.*)\/");
-    console.log(activeURL);
-  });
 });
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+        var activeURL = activeTab.url.match("\/\/(.*?)\/")[1];
+        console.log(activeURL);
+
+        chrome.storage.sync.get('siteLog', function (data) {
+            if (data[activeURL]) {
+                data[activeURL]++;
+            } else {
+                data[activeURL] = 1;
+            }
+            console.log(data);
+        });
+        
+    });
+});
+
+
 
